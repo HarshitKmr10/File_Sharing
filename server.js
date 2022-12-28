9a67f85688c0979da062c93393870ced76ebb9f0
Harshitkmr10 <harshitkmr10@gmail.com>
2022-12-29 04:26:02 +0530

    css added

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const upload = multer({ dest: "uploads" })
  })
 
 app.set("view engine","ejs")
-
+app.use(express.static(__dirname + '/public'));
 app.get("/",(req,res) => {
     res.render("index")
 })
